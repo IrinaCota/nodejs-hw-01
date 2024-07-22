04650c12392f5729452b78eac15352945d65d499
IrinaCota <iryna.kotsa91@gmail.com>
2024-07-22 21:20:30 +0100

    step 2

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,5 +1,24 @@
-import { PATH_DB } from '../constants/contacts.js';
+import { PATH_DB } from '../constants/contacts';
+import fs from 'fs/promises';
+import { createFakeContact } from '../utils/createFakeContact';
 
-const generateContacts = async (number) => {};
+const generateContacts = async (number) => {
+    try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contacts = data ? JSON.parse(data) : [];
+
+    const contactsToAdd = [];
+    for (let i = 0; i < number; i++) {
+      contactsToAdd.push(createFakeContact());
+    }
+
+    await fs.writeFile(
+      PATH_DB,
+      JSON.stringify([...contacts, ...contactsToAdd], undefined, 2),
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 generateContacts(5);
