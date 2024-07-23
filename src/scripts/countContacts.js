4b142d5c5603af5aa8b59ebbdde9a410c32fbda6
IrinaCota <iryna.kotsa91@gmail.com>
2024-07-23 18:24:35 +0100

    task 5-7

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,5 +1,14 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const countContacts = async () => {};
+export const countContacts = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const contactsCount = JSON.parse(data).length;
+    return contactsCount;
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,5 +1,18 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'fs/promises';
 
-export const removeLastContact = async () => {};
+export const removeLastContact = async () => {
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const totalContacts = JSON.parse(data);
+
+    if (totalContacts.length > 0) {
+      totalContacts.pop();
+      fs.writeFile(PATH_DB, JSON.stringify(totalContacts, null, 2));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 removeLastContact();
